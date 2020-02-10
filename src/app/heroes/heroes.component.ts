fe724fc610353fec2b87c0d4b48d99b4656dd79c
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-10 13:18:19 -0600

    Add additional messages to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,11 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-heroes = Hero [];
-selectedHero: Hero;
+  selectedHero: Hero;
+
+  heroes: Hero [];
 
 constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-getHeroes(): void {
-  this.heroService.getHeroes()
-    .subscribe (heroes => this.heroes = heroes);
-}
 
 ngOnInit() {
   this.getHeroes();
@@ -25,7 +21,15 @@ ngOnInit() {
 
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
+
+getHeroes(): void {
+this.heroService.getHeroes()
+  .subscribe (heroes => this.heroes = heroes);
+}
+
+
 }
 
 
