7e03207dc0418e601ccf98bd67768d6e2f665ee9
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-10 10:59:23 -0600

    Inject the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-heroes = HEROES;
+heroes = Hero [];
 selectedHero: Hero;
 
-constructor() {}
+constructor(private heroService: HeroService, private messageService: MessageService) {}
 
 ngOnInit() {}
 
