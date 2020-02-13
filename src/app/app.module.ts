7bd1a3f132196029986a12737dba951d72e08d09
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-13 14:08:06 -0600

    Add RouterOutlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
@@ -9,6 +10,7 @@ import { MessagesComponent } from './messages/messages.component' ; // <-- NgMod
 
 @NgModule({
   declarations: [
+    AppRoutingModule,
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
