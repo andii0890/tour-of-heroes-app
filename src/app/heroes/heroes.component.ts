aad36da12ef6ab0620ee3317e834545278ae6fbf
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-17 13:36:51 -0600

    Delete a Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,6 +29,10 @@ heroes: Hero[];
       this.heroes.push(hero);
     });
   }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
 
 
